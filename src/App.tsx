8a43044c958820eba8734e78fb8c9450c93e6f47
HooraFarhangi <hoora9977@gmail.com>
2025-08-24 17:21:08 +0330

    Filter games by platform

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ import GenreList from "./components/GenreList";
 import { Genre } from "./hooks/useGenres.";
 import { useState } from "react";
 import PlatformSelector from "./components/PlatformSelector";
+import { Platform } from "./hooks/useGames";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
+    null
+  );
 
   return (
     <Grid
@@ -26,8 +30,13 @@ function App() {
       </GridItem>
 
       <GridItem area="main">
-        <PlatformSelector />
-        <GameGrid selectedGenre={selectedGenre} />
+        <PlatformSelector
+          selectedPlatform={selectedPlatform} onSelectPlatfrom={(platform) => setSelectedPlatform(platform)}
+        />
+        <GameGrid
+          selectedGenre={selectedGenre}
+          selectedPlatform={selectedPlatform}
+        />
       </GridItem>
     </Grid>
   );
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,8 +1,14 @@
+import { Platform } from "@/hooks/useGames";
 import usePlatforms from "@/hooks/usePlatforms";
 import { Button, Menu, MenuItem, Portal } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-function PlatformSelector() {
+interface Props {
+  onSelectPlatfrom: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
+}
+
+function PlatformSelector({ onSelectPlatfrom, selectedPlatform }: Props) {
   const { data, error } = usePlatforms();
   if (error) return null;
 
@@ -11,14 +17,18 @@ function PlatformSelector() {
       <Menu.Trigger asChild>
         <Button variant="outline" size="sm">
           <BsChevronDown />
-          Platforms
+          {selectedPlatform?.name || "Platforms"}
         </Button>
       </Menu.Trigger>
       <Portal>
         <Menu.Positioner>
           <Menu.Content>
             {data.map((platform) => (
-              <MenuItem value={platform.name} key={platform.id}>
+              <MenuItem
+                onClick={() => onSelectPlatfrom(platform)}
+                value={platform.name}
+                key={platform.id}
+              >
                 {platform.name}
               </MenuItem>
             ))}
